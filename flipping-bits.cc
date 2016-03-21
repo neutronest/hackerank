32a17eaad7d6f7579abdce06154f77aa2d96a805
neutronest <neutronest@gmail.com>
2016-03-21 22:38:56 +0900

    add flipping-bits

---
+++ flipping-bits.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+
+  int t;
+  unsigned int n;
+  cin>>t;
+  for(auto a0z = 0; a0z < t; ++a0z) {
+    cin>>n;
+    cout<<~n<<endl;
+  }
+
+  return 0;
+}
